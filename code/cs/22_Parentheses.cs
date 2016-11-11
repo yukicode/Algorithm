82afde17c7ad4f3cc82ab7f1595342b0aed8d922
yukicode <yolandaapply@gmail.com>
2016-11-10 23:19:03 -0800

    add leetcode 22, generate parenthesis c# solution

---
+++ code/cs/22_Parentheses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+
+    //Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+    public class Parenthesis
+    {
+        private List<string> all = new List<string>();
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            GenerateParenthesis("", n, n);
+            return all;
+        }
+
+        private void GenerateParenthesis(string s, int left, int right)
+        {
+            if (left > 0)
+            {
+                GenerateParenthesis(s + "(", left - 1, right);
+            }
+            if (left < right)
+            {
+                GenerateParenthesis(s + ")", left, right - 1);
+            }
+            if (left == 0 && right == 0)
+            {
+                all.Add(s);
+            }
+        }
+    }
+}
